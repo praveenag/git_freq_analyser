28dde9dfe3fcd8fadb83aa61cb9cec71767c80ed
Praveena G <praveena.gunasekhar@gmail.com>
2014-10-14 22:09:40 +0100

    crude implementation to get the first graph out.

--- parse.rb
+++ parse.rb
@@ -1,16 +1,19 @@
 $dirs=["/Users/praveeg/projects/sdfe/sdfe-webapp", "/Users/praveeg/projects/sdfe/sdfe-authentication-service"]
 def parse_dirs(dirs)
 	dirs.each do |dir|
+		x_step = 0
+		y_step = 0
 		p "PARSING #{dir}"
 		files=pick_most_used_files(dir)
 		files.each do |file|
-			p "#{date(dir, file[1])} - #{file[1]}"
+			# p "#{date(dir, file[1])} - #{file[0]} - #{file[1]}"
+			puts "[#{x_step+=10}, #{y_step+=10}, #{file[0]}],"
 		end
 	end
 end
 
 def most_used_files(dir)
-	`cd #{dir};git log --pretty=format: --name-only | sort | uniq -c | sort -rg | head -10`.split("\n")
+	`cd #{dir};git log --pretty=format: --name-only | sort | uniq -c | sort -rg | head -20`.split("\n")
 end
 
 
